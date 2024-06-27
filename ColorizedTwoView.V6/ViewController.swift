2cdcb23c3f17c8f0bf53292f446157fb5785f439
MaratXcode <kosjachok8@yandex.ru>
2024-06-27 15:09:21 +0300

    Initial Commit

---
+++ ColorizedTwoView.V6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedTwoView.V6
+//
+//  Created by Marat Fakhrizhanov on 27.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
